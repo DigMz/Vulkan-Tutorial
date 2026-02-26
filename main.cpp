627d488ce5af37e3070f26c64602e933b8a33ded
Diego Martinez <diegomtz1080@gmail.com>
2026-02-25 20:36:23 -0600

    Presentation - Swap Chain

--- main.cpp
+++ main.cpp
@@ -97,6 +97,9 @@ private:
   VkQueue presentQueue; // handle for present queue
 
   VkSwapchainKHR swapChain; // Holds swap chain handle
+  std::vector<VkImage> swapChainImages; // Holds swap chain images
+  VkFormat swapChainImageFormat;
+  VkExtent2D swapChainExtent;
 
   // Create GLFW Window
   void initWindow() {
@@ -359,6 +362,13 @@ private:
     if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
       throw std::runtime_error("failed to create swap chain!");
     }
+
+    vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+    swapChainImages.resize(imageCount);
+    vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+
+    swapChainImageFormat = surfaceFormat.format;
+    swapChainExtent = extent;
   }
 
   VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
