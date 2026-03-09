4effc011f4a48adcb883b7d0796d372a6db6dd37
Diego Martinez <diegomtz1080@gmail.com>
2026-03-08 22:43:15 -0500

    Shell Script Utility Improvement

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,10 +1,14 @@
-project_path="/home/digmz/Documents/C++/Vulkan/Vulkan-Tutorial/"
-shader_path="${project_path%/}/shaders"
+#!/bin/bash
+# Get the absolute path of the script
+SCRIPT=$(readlink -f "$0")
+# Get the directory containing the script
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo "Script directory: $SCRIPTPATH"
 
-shader_vert_path="${shader_path%/}/shader.vert"
-shader_frag_path="${shader_path%/}/shader.frag"
-shader_vert_out="${shader_path%/}/vert.spv"
-shader_frag_out="${shader_path%/}/frag.spv"
+shader_vert_path="${SCRIPTPATH%/}/shader.vert"
+shader_frag_path="${SCRIPTPATH%/}/shader.frag"
+shader_vert_out="${SCRIPTPATH%/}/vert.spv"
+shader_frag_out="${SCRIPTPATH%/}/frag.spv"
 
 glslc "$shader_vert_path" -o "$shader_vert_out"
 glslc "$shader_frag_path" -o "$shader_frag_out"
